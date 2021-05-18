a27691bbb8385ed5c8655ad0b7801e7f20d3e370
326th <minecraftskinman@gmail.com>
2021-05-18 22:19:25 +0700

    custom login don

--- src/main/java/ku/message/config/SecurityConfig.java
+++ src/main/java/ku/message/config/SecurityConfig.java
@@ -33,10 +33,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated();
 
         http.formLogin()
+                .loginPage("/login")
                 .defaultSuccessUrl("/message", true)
-                .and().logout();
-
+                .permitAll()
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .clearAuthentication(true)
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID", "remember-me")
+                .permitAll();
     }
+
 
 
 
